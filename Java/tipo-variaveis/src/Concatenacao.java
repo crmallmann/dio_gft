13d15cf5b94ff5da831fa1a0ce413d2c762f6629
Cibele <cibelermallmann@gmail.com>
2022-12-03 12:30:35 -0300

    java

---
+++ Java/tipo-variaveis/src/Concatenacao.java
@@ -0,0 +1,36 @@
+public class Concatenacao {
+
+    public static void main(String[] args) {
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+                
+    }
+    
+}
--- Java/tipo-variaveis/src/Operadores.java
+++ Java/tipo-variaveis/src/Operadores.java
@@ -1,36 +1,20 @@
+import javax.print.event.PrintJobListener;
+
+import javafx.print.PrintColor;
+
 public class Operadores {
-
-    public static void main(String[] args) {
-
-        //classe Operadores.java
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-		
-        //qual o resultado das expressoes abaixo?
-        String concatenacao ="?"; 
-
-        concatenacao = 1+1+1+"1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+1;
-
-        System.out.println(concatenacao);
-
-
-        concatenacao = 1+"1"+1+"1";
-
-        System.out.println(concatenacao);
-
-
-        concatenacao = "1"+1+1+1;
-
-        System.out.println(concatenacao);
-
-
-        concatenacao = "1"+(1+1+1);
-
-        System.out.println(concatenacao);
-                
-    }
     
+    public static void main(String[] args) {
+        
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+
 }
