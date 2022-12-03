5abaf22218ecd039146dfa3d0a73dfba3a382e0c
Cibele <cibelermallmann@gmail.com>
2022-12-03 19:23:30 -0300

    incremento

---
+++ Java/tipo-variaveis/src/Incremento.java
@@ -0,0 +1,14 @@
+public class Incremento {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //x repetição
+        //numero++;
+        //numero = numero +1;
+
+        System.out.println(++ numero);
+        //System.out.println(numero);
+
+    }
+}
