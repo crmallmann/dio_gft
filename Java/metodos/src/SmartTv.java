b33c984f2a86ab8a4179e49966acd055d0e86eca
Cibele <cibelermallmann@gmail.com>
2022-12-19 20:48:21 -0300

    smartv

--- Java/metodos/src/SmartTv.java
+++ Java/metodos/src/SmartTv.java
@@ -4,11 +4,33 @@ public class SmartTv {
     int canal=1;
     int volume=25;
 
+    
+
     public void ligar(){
         ligada=true;
     }
 
     public void desligar(){
         ligada=false;
+    }
+
+    public void aumentarVolume(){
+        volume++;
+        System.out.println("Aumentando o volume para " + volume);
+    }
+
+    public void diminuirVolume(){
+        volume--;
+        System.out.println("Diminuindo o volume para " + volume);
+    }
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Aumentando o canal para " + canal);
+    }
+
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Diminuindo o canal para " + canal);
     }
 }
