3e6ea4dd27f5d94a644df409b58c78da29f0b3aa
Cibele <cibelermallmann@gmail.com>
2022-12-19 18:19:21 -0300

    incremento

--- Java/tipo-variaveis/src/Incremento.java
+++ Java/tipo-variaveis/src/Incremento.java
@@ -1,14 +1,21 @@
 public class Incremento {
     
     public static void main(String[] args) {
-        int numero = 5;
+        //int numero = 5;
+
+        boolean variavel = true;
 
         //x repetição
         //numero++;
         //numero = numero +1;
 
-        System.out.println(++ numero);
+        variavel = !variavel; //variável altera
+        
+        //System.out.println(++ numero);
         //System.out.println(numero);
-
+        //System.out.println( numero -- );
+        //System.out.println(numero);
+        //System.out.println(!variavel); em memória local
+        System.out.println(variavel);
     }
 }
