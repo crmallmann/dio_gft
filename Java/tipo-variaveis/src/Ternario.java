1c660caa8443bbb1d739fbf7bb04010e89f9e8ac
Cibele <cibelermallmann@gmail.com>
2022-12-19 18:29:58 -0300

    ternario

---
+++ Java/tipo-variaveis/src/Ternario.java
@@ -0,0 +1,25 @@
+
+public class Ternario {
+
+   public static void main(String[] args) {
+
+      int a, b;
+
+      a = 5;
+      b = 6;
+      
+      //String resultado = "";   
+      //if(a==b)
+      //   resultado = "verdadeiro";
+      //else
+      //   resultado = "falso";
+
+      String resultado = a==b ?"verdadeiro" : "falso";
+   
+   
+      System.out.println(resultado);
+      
+   }
+ 
+    
+}
