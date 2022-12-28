f46c966e594c20a1e53cf95144b291c3bc4c7523
Cibele <cibelermallmann@gmail.com>
2022-12-28 20:52:10 -0300

    first test

--- Java/pratice-automation/src/test/java/dev/cibele/AppTest.java
+++ Java/pratice-automation/src/test/java/dev/cibele/AppTest.java
@@ -2,8 +2,23 @@ package dev.cibele;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AppTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void helloSelenium() {
+        //https://www.selenium.dev/documentation/webdriver/getting_started/install_drivers/
+        System.getProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/");
+
+        driver.quit();
+    }
 
     @Test
     public void shouldAnswerWithTrue() {
